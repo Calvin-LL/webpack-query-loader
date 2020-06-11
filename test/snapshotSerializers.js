9f9e57627f75a3cbbbf110c15d748d0951d4210f
Calvin Liang <me@calvin.sh>
2020-06-11 15:55:36 -0700

    test: remove user directory from snapshots

--- test/snapshotSerializers.js
+++ test/snapshotSerializers.js
@@ -4,9 +4,15 @@ const projectRoot = path.resolve(__dirname, "..");
 
 module.exports = {
   print: function print(val) {
-    return `"${val.replace(projectRoot, "<rootDir>")}"`;
+    if (typeof val === "string")
+      return `"${val.replace(new RegExp(projectRoot, "g"), "<rootDir>")}"`;
+    if (val instanceof Error)
+      return `[${val.name}: ${val.message.replace(
+        new RegExp(projectRoot, "g"),
+        "<rootDir>"
+      )}]`;
   },
   test(val) {
-    return typeof val === "string";
+    return typeof val === "string" || val instanceof Error;
   },
 };
